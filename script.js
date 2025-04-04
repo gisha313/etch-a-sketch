3d17e31d76b5b295eb23d40c89025b57e8840a76
gisha <52324253+gisha313@users.noreply.github.com>
2025-04-04 19:21:21 +0200

    Add ability to color 'pixels' on hover

--- script.js
+++ script.js
@@ -4,11 +4,19 @@ function createGrid(n) {
     for (let i = 0; i < n*n; i++){
         let pixel = document.createElement("div");
         pixel.classList.add("pixel");
+
         let heightPercentage = (1.0 / n * 100).toFixed(2).toString() + "%"
-        console.log(heightPercentage)
         pixel.style.height = heightPercentage;
+
+        pixel.addEventListener("mouseenter", () => {
+            colorPixel(pixel);
+        });
         grid.appendChild(pixel);
     }
+}
+
+function colorPixel(pixel) {
+    pixel.style.backgroundColor = "black";
 }
 
 createGrid(100);
